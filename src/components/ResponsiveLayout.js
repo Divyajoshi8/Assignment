9aea6ef62e81278ac8e34464fa5b8eaed7c34f26
Divyajoshi8 <97658852+Divyajoshi8@users.noreply.github.com>
2024-09-15 13:57:22 +0530

    contractors and vendor

--- src/components/ResponsiveLayout.js
+++ src/components/ResponsiveLayout.js
@@ -22,8 +22,23 @@ const ResponsiveLayout = () => {
   const [showDetails, setShowDetails] = useState(null); 
   const [showSuccess, setShowSuccess] = useState(false);
   const [vendors, setVendors] = useState([]); // State for vendor data
+  const [Contractors, setContractors] = useState([]); // State for vendor data
   const [showForm, setShowForm] = useState(false);
   const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    phoneNumber: '',
+    comingFrom: '',
+    company: '',
+    hostName: '',
+    purposeOfVisit: '',
+    idType: '',
+    idNumber: '',
+    dateOfVisit: '',
+    entryTime:'',
+    exitTime:'',
+  });
+  const [formDataContractor, setFormDataContractor] = useState({
     fullName: '',
     email: '',
     phoneNumber: '',
@@ -42,6 +57,10 @@ const ResponsiveLayout = () => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
+  const handleInputChangeContractor = (e) => {
+    const { name, value } = e.target;
+    setFormDataContractor({ ...formDataContractor, [name]: value });
+  };
 
 
   const handleSubmit = (e) => {
@@ -50,6 +69,33 @@ const ResponsiveLayout = () => {
     setVendors([...vendors, formData]);
     // Clear the form data
     setFormData({
+      fullName: '',
+      email: '',
+      phoneNumber: '',
+      comingFrom: '',
+      company: '',
+      hostName: '',
+      purposeOfVisit: '',
+      idType: '',
+      idNumber: '',
+      dateOfVisit: '',
+      entryTime:'',
+      exitTime:'',
+    });
+    // Show success message
+    setShowSuccess(true);
+    // Hide the form
+    setShowForm(false);
+    // Hide the success message after 3 seconds
+    setTimeout(() => setShowSuccess(false), 3000);
+  };
+
+  const handleSubmitContractor = (e) => {
+    e.preventDefault();
+    // Add the new vendor to the list
+    setContractors([...Contractors, formDataContractor]);
+    // Clear the form data
+    setFormDataContractor({
       fullName: '',
       email: '',
       phoneNumber: '',
@@ -686,6 +732,418 @@ const ResponsiveLayout = () => {
         <td>{vendor.purposeOfVisit}</td>
         <td>{vendor.idNumber}</td>
         <td>{vendor.exitTime}</td>
+        <td style={{ textAlign: 'center', verticalAlign: 'middle' }}> {/* Center the icon */}
+          <FaEye
+            style={{ cursor: 'pointer', color: showDetails === index ? '#FF8C00' : 'gray' }}
+            onClick={() => handleToggleDetails(index)}
+          />
+          {showDetails === index && (
+            <span style={{ color: '#FF8C00' }}></span>
+          )}
+          {showDetails !== index && (
+            <span style={{ color: '#999' }}></span>
+          )}
+        </td>
+      </tr>
+    ))}
+  </tbody>
+</Table>
+
+
+                </div>
+              )}
+
+{content === 'contractor' && (
+                <div>
+                  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <h2 style={{ fontSize: '20px', whiteSpace:'nowrap' }}>List of Contractors</h2>
+                    <button
+                      className="add-new-btn"
+                      style={{
+                        backgroundColor: '#FF8C00',
+                        color: 'white',
+                        border: 'none',
+                        padding: '10px 20px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        cursor: 'pointer',
+                        borderRadius: '5px',
+                        marginLeft: '89%',
+                        width: 'auto',
+                        height:'40px',
+                        whiteSpace: 'nowrap' // Prevent text from wrapping
+                      }}
+                      onClick={() => setShowForm(true)}
+                    >
+                      <FaPlus style={{ marginRight: '5px' }} /> Add New
+                    </button>
+
+      {showForm && (
+        <div
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'white',
+            padding: '30px',
+            borderRadius: '10px',
+            boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.2)',
+            width: '600px',
+            zIndex: 1000,
+          }}
+        >
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <h2 style={{ fontSize: '20px', color: '#FF8C00' }}>Add Contractors</h2>
+            <FaTimes
+              onClick={() => setShowForm(false)}
+              style={{ cursor: 'pointer', fontSize: '20px', color: '#FF8C00' }}
+            />
+          </div>
+          <form onSubmit={handleSubmitContractor}>
+            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+              <div style={{ flex: '1 1 30%' }}>
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  name="fullName"
+                  value={formDataContractor.fullName}
+                  onChange={handleInputChangeContractor}
+                  style={{
+                    width: '100%',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    border: '1px solid #FF8C00',
+                  }}
+                />
+              </div>
+              <div style={{ flex: '1 1 30%' }}>
+                <label>Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formDataContractor.email}
+                  onChange={handleInputChangeContractor}
+                  style={{
+                    width: '100%',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    border: '1px solid #FF8C00',
+                  }}
+                />
+              </div>
+              <div style={{ flex: '1 1 30%' }}>
+                <label>Phone Number</label>
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  value={formDataContractor.phoneNumber}
+                  onChange={handleInputChangeContractor}
+                  style={{
+                    width: '100%',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    border: '1px solid #FF8C00',
+                  }}
+                />
+              </div>
+
+              {/* Row 2 */}
+              <div style={{ flex: '1 1 30%' }}>
+                <label>Coming From</label>
+                <input
+                  type="text"
+                  name="comingFrom"
+                  value={formDataContractor.comingFrom}
+                  onChange={handleInputChangeContractor}
+                  style={{
+                    width: '100%',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    border: '1px solid #FF8C00',
+                  }}
+                />
+              </div>
+              <div style={{ flex: '1 1 30%' }}>
+                <label>Company</label>
+                <input
+                  type="text"
+                  name="company"
+                  value={formDataContractor.company}
+                  onChange={handleInputChangeContractor}
+                  style={{
+                    width: '100%',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    border: '1px solid #FF8C00',
+                  }}
+                />
+              </div>
+              <div style={{ flex: '1 1 30%' }}>
+                <label>Host Name</label>
+                <input
+                  type="text"
+                  name="hostName"
+                  value={formDataContractor.hostName}
+                  onChange={handleInputChangeContractor}
+                  style={{
+                    width: '100%',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    border: '1px solid #FF8C00',
+                  }}
+                />
+              </div>
+
+              {/* Row 3 */}
+              <div style={{ flex: '1 1 30%' }}>
+                <label>Purpose of Visit</label>
+                <input
+                  type="text"
+                  name="purposeOfVisit"
+                  value={formDataContractor.purposeOfVisit}
+                  onChange={handleInputChangeContractor}
+                  style={{
+                    width: '100%',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    border: '1px solid #FF8C00',
+                  }}
+                />
+              </div>
+              <div style={{ flex: '1 1 30%' }}>
+                <label>ID Type</label>
+                <input
+                  type="text"
+                  name="idType"
+                  value={formDataContractor.idType}
+                  onChange={handleInputChangeContractor}
+                  style={{
+                    width: '100%',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    border: '1px solid #FF8C00',
+                  }}
+                />
+              </div>
+              <div style={{ flex: '1 1 30%' }}>
+                <label>ID Number</label>
+                <input
+                  type="text"
+                  name="idNumber"
+                  value={formDataContractor.idNumber}
+                  onChange={handleInputChangeContractor}
+                  style={{
+                    width: '100%',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    border: '1px solid #FF8C00',
+                  }}
+                />
+              </div>
+
+              {/* Row 4 */}
+              <div style={{ flex: '1 1 30%' }}>
+                <label>Date of Visit</label>
+                <input
+                  type="date"
+                  name="dateOfVisit"
+                  value={formDataContractor.dateOfVisit}
+                  onChange={handleInputChangeContractor}
+                  style={{
+                    width: '100%',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    border: '1px solid #FF8C00',
+                  }}
+                />
+              </div>
+
+              <div style={{ flex: '1 1 30%' }}>
+    <label>Entry Time</label>
+    <input
+      type="time"
+      name="entryTime"
+      value={formDataContractor.entryTime}
+      onChange={handleInputChangeContractor}
+      style={{
+        width: '100%',
+        padding: '10px',
+        borderRadius: '5px',
+        border: '1px solid #FF8C00',
+      }}
+    />
+  </div>
+
+  <div style={{ flex: '1 1 30%' }}>
+    <label>Exit Time</label>
+    <input
+      type="time"
+      name="exitTime"
+      value={formDataContractor.exitTime}
+      onChange={handleInputChangeContractor}
+      style={{
+        width: '100%',
+        padding: '10px',
+        borderRadius: '5px',
+        border: '1px solid #FF8C00',
+      }}
+    />
+  </div>
+            </div>
+
+            <button
+              type="submit"
+              style={{
+                backgroundColor: '#FF8C00',
+                color: 'white',
+                border: 'none',
+                padding: '10px 20px',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                marginTop: '20px',
+                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)',
+              }}
+            >
+              Submit
+            </button>
+          </form>
+          </div>
+      )}
+
+{showSuccess && (
+                      <div
+                        style={{
+                          position: 'fixed',
+                          top: '10%',
+                          left: '50%',
+                          transform: 'translateX(-50%)',
+                          backgroundColor: '#d4edda',
+                          color: '#155724',
+                          padding: '10px 20px',
+                          borderRadius: '5px',
+                          boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)',
+                          zIndex: 1000,
+                        }}
+                      >
+                        Successfully submitted!
+                      </div>
+                    )}
+
+                    
+                  </div>
+
+ {/* Pagination Section */}
+ <div style={{ marginTop: '20px' , display: 'flex', alignItems: 'center' }}>
+                      <p style={{ marginRight: '10px', fontSize: '16px' }}>Page:</p>
+                      <div style={{ position: 'relative', display: 'inline-flex', alignItems: 'center' }}>
+                        <input
+                          type="number"
+                          value={pageNumber}
+                          onChange={(e) => setPageNumber(parseInt(e.target.value))}
+                          style={{
+                            width: '40px',
+                            height: '30px',
+                            textAlign: 'center',
+                            borderRadius: '5px',
+                            border: '1px solid #ccc',
+                            fontSize: '16px',
+                          }}
+                        />
+                        <div style={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          position: 'absolute',
+                          right: '-20px',
+                          top: '50%',
+                          transform: 'translateY(-50%)',
+                        }}>
+                          <FaChevronUp
+                            onClick={() => handlePageChange('up')}
+                            style={{
+                              cursor: 'pointer',
+                              fontSize: '14px',
+                              color: '#FF8C00',
+                              marginBottom: '5px',
+                            }}
+                          />
+                          <FaChevronDown
+                            onClick={() => handlePageChange('down')}
+                            style={{
+                              cursor: 'pointer',
+                              fontSize: '14px',
+                              color: '#FF8C00',
+                            }}
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+
+                  {/* Search Box */}
+                  <div style={{ marginTop: '-39px', position: 'relative', width: '100px', marginLeft: '95%' }}>
+  <input
+    type="text"
+    value={searchInput}
+    onChange={(e) => setSearchInput(e.target.value)}
+    placeholder="Search"
+    style={{
+      width: '200%',
+      height: '30px',
+      padding: '10px',
+      borderRadius: '30px',
+      outline: 'none', // Remove default outline
+      backgroundColor: '#faf4f0', // Light orange shade
+      border: '1px transparent rgba(245, 211, 186, 0.5)', // Transparent version of the background color
+      paddingLeft: searchInput ? '10px' : '35px', // Adjust padding when typing
+      fontSize: '16px',
+    }}
+  />
+  {!searchInput && (
+    <FaSearch
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '10px',
+        transform: 'translateY(-50%)',
+        color: '#999',
+      }}
+    />
+  )}
+</div>
+
+               {/* Table */}
+<Table bordered hover style={{ marginTop: '20px', borderColor: '#fffff4', width: '195vh', borderCollapse: 'collapse' }}>
+  <thead>
+    <tr style={{ height: '65px', textAlign: 'center' }}>
+      <th style={{ backgroundColor: 'orange', color: 'white' }}>Name of the Vendor</th>
+      <th style={{ backgroundColor: 'orange', color: 'white' }}>Contact Number</th>
+      <th style={{ backgroundColor: 'orange', color: 'white' }}>Date of Visit</th>
+      <th style={{ backgroundColor: 'orange', color: 'white' }}>Entry Time</th>
+      <th style={{ backgroundColor: 'orange', color: 'white' }}>Visiting Company</th>
+      <th style={{ backgroundColor: 'orange', color: 'white' }}>Type of Work</th>
+      <th style={{ backgroundColor: 'orange', color: 'white' }}>ID Number</th>
+      <th style={{ backgroundColor: 'orange', color: 'white' }}>Exit Time</th>
+      <th style={{ backgroundColor: 'orange', color: 'white' }}>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {Contractors.map((contractor, index) => (
+      <tr
+        key={index}
+        style={{
+          backgroundColor: index % 2 === 0 ? '#faf4f0' : '#f5d3ba',
+          height: '65px',
+        }}
+      >
+        <td>{contractor.fullName}</td>
+        <td>{contractor.phoneNumber}</td>
+        <td>{contractor.dateOfVisit}</td>
+        <td>{contractor.entryTime}</td>
+        <td>{contractor.company}</td>
+        <td>{contractor.purposeOfVisit}</td>
+        <td>{contractor.idNumber}</td>
+        <td>{contractor.exitTime}</td>
         <td style={{ textAlign: 'center', verticalAlign: 'middle' }}> {/* Center the icon */}
           <FaEye
             style={{ cursor: 'pointer', color: showDetails === index ? '#FF8C00' : 'gray' }}
